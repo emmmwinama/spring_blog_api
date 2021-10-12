eba70988b54b436e17d39eb0d212034321cf63f9
Emmanuel Mwinama <bit15-emwinama@poly.ac.mw>
2021-10-12 10:31:14 +0200

    Initial package -- Untouched

---
+++ src/main/java/tech/mwinama/spring_blog_api/SpringBlogApiApplication.java
@@ -0,0 +1,13 @@
+package tech.mwinama.spring_blog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogApiApplication.class, args);
+	}
+
+}
